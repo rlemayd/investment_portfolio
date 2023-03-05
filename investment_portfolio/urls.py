af5db2f4768dd5f76ea0f324f5fb22c95b1f0019
Richard Le May <ralemay@miuandes.cl>
2023-03-05 00:22:40 -0300

    Removed test directory

--- investment_portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from portfolio_manager.views import *
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('portfolio_manager.urls')),
-]
